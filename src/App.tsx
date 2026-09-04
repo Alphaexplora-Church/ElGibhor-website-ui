2ad45d695e671434d69802dd17ef066b47eafe06
Lance <lancepiastroxx19@gmail.com>
2026-09-04 13:22:41 +0800

    Revert "Add Library Page"
    This reverts commit 8a0ffdde594866e02b2108d35674ba2bf636eef2.

--- src/App.tsx
+++ src/App.tsx
@@ -15,8 +15,9 @@ const AboutUs = lazy(() => import('./features/about/views/AboutUs').then(m => ({
 const Experience = lazy(() => import('./features/experience/views/Experience').then(m => ({ default: m.Experience })));
 const Give = lazy(() => import('./features/give/views/Give').then(m => ({ default: m.Give })));
 const Engage = lazy(() => import('./features/engage/views/Engage').then(m => ({ default: m.Engage })));
-const Watch = lazy(() => import("./features/watch/views/Library").then((m) => ({ default: m.Library })));
-const Library = lazy(() => import('./features/watch/views/Library').then(m => ({ default: m.Library })));
+const Watch = lazy(() => import('./features/watch/views/Watch').then(m => ({ default: m.Watch })));
+const PrayerWall = lazy(() => import('./features/prayer/views/PrayerWall').then(m => ({ default: m.PrayerWall })));
+const Ministry = lazy(() => import('./features/ministries/views/Ministry').then(m => ({ default: m.Ministry })));
 
 // Lazy loading placeholder components
 const AlterOne = lazy(() => import('./features/about/views/alterone').then(m => ({ default: m.AlterOne })));
@@ -52,7 +53,8 @@ const AnimatedRoutes = () => {
         <Route path="/give" element={<Give />} />
         <Route path="/engage" element={<Engage />} />
         <Route path="/watch" element={<Watch />} />
-        <Route path="/library" element={<Library />} />
+        <Route path="/prayer" element={<PrayerWall />} />
+        <Route path="/ministries" element={<Ministry />} />
 
         {/* Placeholder/Test Routes */}
         <Route path="/alter-one" element={<AlterOne />} />
